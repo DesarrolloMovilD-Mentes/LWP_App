f217da90ba4938306f49d4e51202dc0227a7b954
unknown <semataoe@est.utn.ac.cr>
2023-12-03 02:01:12 -0600

    Some Changes to comment section

--- lwp_lab01/app/src/main/java/com/example/lwp_lab01/ui/PDF_List/PDF_Detail.kt
+++ lwp_lab01/app/src/main/java/com/example/lwp_lab01/ui/PDF_List/PDF_Detail.kt
@@ -1,15 +1,21 @@
 package com.example.lwp_lab01.ui.PDF_List
 
+import android.content.ContentValues.TAG
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 import com.example.lwp_lab01.R
+import com.example.lwp_lab01.entities.cls_PDFs
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class PDF_Detail : AppCompatActivity() {
@@ -20,7 +26,8 @@ class PDF_Detail : AppCompatActivity() {
     private lateinit var textDate: TextView
     private lateinit var textInfoPdf: TextView
     private lateinit var commentEditText: EditText
-
+    var db = FirebaseFirestore.getInstance()
+    var auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdf_detail)
@@ -56,5 +63,28 @@ class PDF_Detail : AppCompatActivity() {
 //
 //        val date: String = timestampToString(intent.extras!!.getLong("postDate"))
 //        txtPostDateName.setText(date)
+    }
+    private fun obtenerDatos() {
+        //Toast.makeText(this,"Esperando hacer algo importante", Toast.LENGTH_LONG).show()
+        var coleccion: ArrayList<cls_PDFs?> = ArrayList()
+        var listaView: RecyclerView = findViewById(R.id.recyclerView)
+        db.collection("PDF´s_modules").orderBy("CategoryID")
+            .get()
+            .addOnCompleteListener { docc ->
+                if (docc.isSuccessful) {
+                    for (document in docc.result!!) {
+                        Log.d(TAG, document.id + " => " + document.data)
+                        //var datos: cls_PDFs = cls_PDFs(document.data["CategoryID"].toString().toInt(),
+                          //  document.data["CategoryName"].toString(),
+                           // document.data["Description"].toString(),
+                            //document.data["urlImage"].toString())
+                        //coleccion.add(datos)
+                    }
+                    //var adapter: CategoryAdapter = CategoryAdapter(this, coleccion)
+                    //listaView.adapter =adapter
+                } else {
+                    Log.w(TAG, "Error getting documents.", docc.exception)
+                }
+            }
     }
 }
