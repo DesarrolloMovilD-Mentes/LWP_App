78a2bcd70e0fb4174fec0f9cd262a72f445c2086
unknown <semataoe@est.utn.ac.cr>
2023-12-05 22:48:44 -0600

    Comment Section 1.0

--- lwp_lab01/app/src/main/java/com/example/lwp_lab01/ui/slideshow/SlideshowFragment.kt
+++ lwp_lab01/app/src/main/java/com/example/lwp_lab01/ui/slideshow/SlideshowFragment.kt
@@ -1,18 +1,15 @@
 package com.example.lwp_lab01.ui.slideshow
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.lwp_lab01.databinding.FragmentSlideshowBinding
 import com.example.lwp_lab01.R
-import com.example.lwp_lab01.ui.PDF_List.List_PDF
+import com.example.lwp_lab01.ui.comment_List.CommentSection
 
 class SlideshowFragment : Fragment() {
 
@@ -33,7 +30,7 @@ class SlideshowFragment : Fragment() {
         val moduleEnglish = root.findViewById<ImageView>(R.id.englishModule)
 
         moduleEnglish.setOnClickListener{
-            val EModule = Intent(context, List_PDF::class.java)
+            val EModule = Intent(context, CommentSection::class.java)
             startActivity(EModule)
         }
 
